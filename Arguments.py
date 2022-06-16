8acc346120072d0d55458fb4d661a960f5f307e9
allamatti98 <dayanallamatti98@gmail.com>
2022-06-16 08:56:09 +0300

    Adding Arguments file

---
+++ Arguments.py
@@ -0,0 +1,22 @@
+#define a function that takes in arguments then print the output
+def car(make,color,year):
+    print(make,color,year)
+
+car("Mitsubishi","Blue", 1998)
+
+
+def bio(fname, lname,sex,age):
+    print(f"Hello {fname} {lname}, congs on {age} years of positive existance... Cheers to {sex}s")
+bio("Peter", "Parker","Male",23)
+def geo (city,country):
+    print(city + " is a city in " + country)
+
+
+geo("Kampala","Uganda")
+
+
+def automobiles(car,color,model):
+    print(f"I also drive a{color} {car} {model} model!!! ")
+
+
+automobiles('Tesla','blue',2020)

59889a682e00e27809466bf3ae920a8bf27612d9
allamatti98 <dayanallamatti98@gmail.com>
2022-06-21 21:12:40 +0300

    Various uses of Classes

---
+++ Classes ST.py
@@ -0,0 +1,146 @@
+#Create a class with atleast three methods, Create an Object (instance of a class), 
+#Print Functions(methods) & Attributes
+#Create a Constructor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+class Quickie:
+    def read(word):
+        print("Reading data...")
+    def process(word):
+        print("Processing data...")
+
+    def write(word):
+        print("Writing data...")
+
+    def __init__ (self,name,city,country):
+        self.name = name,
+        self.city = city,
+        self.country = country
+Q1 = Quickie("Peter", "Kampala","Uganda")
+
+
+Quickie.read("Cool Boy")
+#Quickie.read()
+Quickie.process("soot")
+Quickie.write("didi")
+
+Quickie.f1 = 1
+Quickie.f2 = 2
+print(Quickie.f1)
+'''
+
+
+
+
+"""
+class Scan:
+    def read(self):
+        print("Reading data...")
+    
+    def process(self):
+        print("Processing")
+    
+    def write(self):
+        print("Writing data")
+
+
+
+#An object is an instance of a class
+Scan1 = Scan()
+
+#print methods
+Scan1.read()
+Scan1.process()
+Scan1.write()
+
+#print Attributes
+Scan1.x = 10
+Scan1.y = 29
+print(Scan1.x)
+
+Scan2 = Scan()
+Scan2.x= 25
+Scan2.y = 45
+print(Scan2.x)
+
+
+class Shujaa:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+Sha1 = Shujaa(6,2,7)
+print(Sha1.x)
+
+
+class shamra:
+    def read(self):
+        print("Reading...")
+    
+    def process(self):
+        print("Processing data...")
+    
+    def write(self):
+        print("Writing data.")
+
+#Object
+sh1 = shamra()
+
+sh1.read()
+sh1.process()
+sh1.write()
+
+#Attributes
+sh2 = shamra()
+sh2.x = 36
+sh2.y = 48
+sh2.z = 79
+print(sh2.y)
+
+#Constructor
+class zedo:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+zd1 = zedo(32,45,79)
+print(zd1.y)
+
+class Cars:
+    def Lambo(self):
+        print("Lambo")
+    def Ferrari(self):
+        print("Ferrari")
+    def McLaren(self):
+        print("McLaren")
+
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+L1 = Cars("s","g","f")
+
+
+L1.Ferrari()
+L1.x = 1
+print(L1.x)
+
+"""

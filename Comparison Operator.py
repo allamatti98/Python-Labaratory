42f1b8f0f2c630500366a4788ad5b00d8db67372
allamatti98 <dayanallamatti98@gmail.com>
2022-06-22 22:50:43 +0300

    A progran that takes in arguments and prints them

---
+++ Comparison Operator.py
@@ -0,0 +1,62 @@
+#Program that accepts names between 3 to 15 characters.
+#Program that accepts strings of specific number of characters eg 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def username_checker():
+    while True:
+        name = input("Type in a username: ")
+        if len(name) < 3:
+            print("Username must be atleast 3 characters long.")
+        elif len(name) > 30:
+            print("Username too long, must be less than 30 characters.")
+        else:
+            print("Name looks good.")
+
+
+def lang_qtn():
+    while True:
+        lan = input("What is the best programming language?: ")
+        if len(lan) != 6:
+            print("Wrong answer.")
+        else:
+            print("Yes, correct answer.")
+            break
+
+
+class ComparisonOperators:
+    def name_check(name):
+        if len(name) < 3:
+            print("Name must be greater than 3 characters and less than 15 characters")
+        elif len(name) > 15:
+            print("Name must be greater than 3 characters and less than 15 characters")
+        else:
+            print("Name looks good.")
+
+    def specific(language):
+        if len(language) != 6:
+            print("Wrong answer")
+        else:
+            print("Elegantooo!!!")
+
+ComparisonOperators.name_check("Big Joe")
+ComparisonOperators.specific("Python")
+"""
+
+#1. username_checker()
+#2. lang_qtn()

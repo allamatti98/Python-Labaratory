1c1825071f3faa9fb614f1cfa5438a7ef246c9f8
allamatti98 <dayanallamatti98@gmail.com>
2022-06-26 06:38:03 +0300

    A program for a dice game

---
+++ Dice.py
@@ -0,0 +1,35 @@
+import random
+def dice_roll():
+    x = random.randint(1,6)
+    y = random.randint(1,6)
+    print(f"({x},{y})")
+dice_roll()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+import random
+class Dice:
+    def roll_dice():
+        x = random.randint(1,6)
+        y = random.randint(1,6)
+        print(f"({x},{y})")
+
+Dice.roll_dice()
+from Speed.RollDice import Dice
+Dice.roll()
+"""

853fb84f6997eb4900e642772089b6a504e2967f
allamatti98 <dayanallamatti98@gmail.com>
2022-06-27 20:42:34 +0300

    A program that prints digits in words

---
+++ Dictionary.py
@@ -0,0 +1,37 @@
+biodata = {
+    "First_Name": "Alex",
+    "Last_Name": "Hunter",
+    "Age": 23,
+    "Striker": True
+}
+
+print(biodata)
+print(biodata["Striker"])
+print(biodata["Last_Name"])
+#print(biodata["School"])
+print(biodata.get("birthdate","1st July 1999"))
+
+
+#Challenge
+numbers = {
+    "0": "Zero ",
+    "1": "One ",
+    "2": "Two ",
+    "3": "Three ",
+    "4": "Four ",
+    "5": "Five ",
+    "6": "Six ",
+    "7": "Seven ",
+    "8": "Eight ",
+    "9": "Nine "
+}
+contact = input("Kindly enter your contact: ")
+for number in contact:
+    print(numbers[number])
+
+#MstAns
+output = ""
+for number in contact:
+    output += numbers.get(number, "!") + " "
+print(output)
+    

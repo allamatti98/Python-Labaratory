4ac188aac8d8dd5f297e4802536bc2f5a67e21b1
allamatti98 <dayanallamatti98@gmail.com>
2022-07-02 04:25:29 +0300

    To prevent programS from crashing due to errors

---
+++ Exceptions.py
@@ -0,0 +1,43 @@
+#Define a function that collects biodata in their correct data types or else it displays your error message.
+def bio():
+    name = input("Name: ")
+    age = int(input("Age: "))
+    nationality = input("Nationality: ")
+    birth = 2022 - age
+try:
+    bio()
+    print("All is well")
+except ValueError:
+    print("Must be a number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def bio():
+    fname = input("What is your first name?: ")
+    lname = input ("What is you second name?: ")
+    age = int(input("How old are you?: "))
+    gender = input("What is your gender?: ")
+    status = bool(input("Are you married?: "))
+    year = 2022
+    birth = year - age
+    print(f"You were born in {birth}???")
+print("Hey there...")
+try:
+    bio()
+    print("Thank You.")
+except ValueError:
+    print("Age must be a number.")
+"""

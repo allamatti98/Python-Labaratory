0fad18442f03abdedd94eadc16cd32692e8378a6
allamatti98 <dayanallamatti98@gmail.com>
2022-07-07 03:40:55 +0300

    Python code dealing with for loops

---
+++ For Loops.py
@@ -0,0 +1,92 @@
+#Print every item in the objects.
+#Find the total price of items in a list.
+
+
+
+
+
+
+
+
+
+
+
+"""
+tuple = (1,2,3,4,5,6)
+numbers = [1,2,3,4,5,6,7,8,9]
+String = "Python"
+listrnge = range(20)
+lists = ["Dayan", "John", "Peter"]
+sentences = "How is that side mate?"
+for no in tuple:
+    print (no)
+print("Done")
+jj =sentences.split(" ")
+for words in jj:
+    sentences.replace()
+sum = 0
+for no in numbers:
+    sum += no
+print(sum)
+
+
+print(String.replace("P","J"))
+print("Done")
+
+
+for name in lists:
+    print (name)
+
+mg = sentences.split(" ")
+for words in mg:
+    print(words)
+print(sentences.split())
+
+print(lists[0:3])
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for number in numbers:
+    print(number)
+print("END")
+String = "Pyhton"
+for letters in String:
+    print(letters)
+print("END")
+Lists = ["London", "Miami", "Los Angeles"]
+for city in Lists:
+    print(city)
+print("END")
+print(numbers[:8])
+print(String[1:-1])
+print(String.replace('P','J'))
+prices = (10,20,30)
+sum = 0
+for cost in prices:
+    sum = cost + sum
+print(f"The total price is {sum}")
+
+tuple = (1,2,3,4,5,6)
+numbers = [1,2,3,4,5,6,7,8,9]
+String = "Python"
+listrnge = range(20)
+lists = ["Dayan", "John", "Peter"]
+sentences = "How is that side mate?"
+for no in tuple:
+    print(no)
+for no in numbers:
+    print(no)
+for letter in String:
+    print(letter)
+for no in listrnge:
+    print (no)
+for word in lists:
+    print(word)
+words = sentences.split()
+for word in words:
+    print(word)
+print(sentences.replace('How','Wagwan'))
+
+total = 0
+for nos in numbers:
+    total += nos
+print(total)
+"""

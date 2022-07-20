15fe06b1d9b8e4f26755f10ba6e6eb7c0b285a6f
allamatti98 <dayanallamatti98@gmail.com>
2022-07-20 09:56:13 +0300

    Added the guessing game

---
+++ Guessing Game.py
@@ -0,0 +1,31 @@
+#Make a program that gives a player 3 chances to guess a magic number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+for i in range (3):
+    cc = int(input("Guess the magic number: "))
+    if cc != 9:
+        print("Sorry wrong answer.")
+    else:
+        print("Congz... You nailed it.")
+        break
+else:
+    print("Sorry chances are done")
+
+'''

7892156253a6e0fa871e2d2eaf321885dff3ea3d
allamatti98 <dayanallamatti98@gmail.com>
2022-07-21 16:50:14 +0300

    Lists

---
+++ Lists.py
@@ -0,0 +1,17 @@
+ballers = ["Kevin", "Sterling", "Grielish", "Fernandinho"]
+print (ballers [0])
+print (ballers[0:3])
+print (ballers [-3])
+ballers [0] = "Kevin De Bruyne"
+print (ballers)
+for Item in ballers:
+    print(Item)
+
+
+sets = [1,5,2,3,5,6,3,7,8,9,9,9,6,4,3,6,7,9,11]
+import math
+from statistics import mode
+print(max(sets))
+print(mode(sets))
+
+numbers = (1,2,3,4,5,6,7,8,9,10)

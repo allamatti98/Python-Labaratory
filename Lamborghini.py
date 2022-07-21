4f2b431ea06f4190554e89af17f61a26dd52a263
allamatti98 <dayanallamatti98@gmail.com>
2022-07-21 16:47:22 +0300

    Lamborghini function

---
+++ Lamborghini.py
@@ -0,0 +1,11 @@
+class Lambo:
+    def lambo_diagnosis():
+        print("""
+        Dashboard Lights on
+        Speedometer run
+        3d Model off Lambo
+    -> Fuel Low
+    -> Next Service in 12 days
+        Headlights on
+        Radio On
+    """)

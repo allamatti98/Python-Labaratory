b95b9bd889d7e6746944f81d698eea4503081c4e
allamatti98 <dayanallamatti98@gmail.com>
2022-07-24 23:14:48 +0300

    math functions

---
+++ Math functions.py
@@ -0,0 +1,10 @@
+x = 57.98
+print(round(x)) #Rounds off
+y = x * -10
+print(y) 
+print(abs(y)) #positive answer always
+import math
+w = 5.6
+print(math.ceil(w))
+print(math.floor(w))
+print(math.cos(w))

e7e427cf5cc3e7e80c95d08741a7bbf539bdef86
allamatti98 <dayanallamatti98@gmail.com>
2022-07-24 23:16:21 +0300

    added modules

---
+++ Modules.py
@@ -0,0 +1,13 @@
+from audioop import findmax
+import TempConverterFunctions
+print(TempConverterFunctions.Celcius_to_Farenheight(22))
+print(TempConverterFunctions.Farenheight_to_Celcius(94))
+
+import UtilsModules
+numbers = [2,4,5,7,8,3,4,6,8,9]
+maxim = findmax(numbers)
+print(maxim)
+
+from TempConverterFunctions import Celcius_to_Farenheight
+print(Celcius_to_Farenheight(30))
+

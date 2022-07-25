ea27770983353b847a6ba7b3691e97c723566edc
allamatti98 <dayanallamatti98@gmail.com>
2022-07-25 08:28:48 +0300

    Nested loops

---
+++ Nested Loops ST.py
@@ -0,0 +1,20 @@
+from Speed.Mathematics import find_max_number
+
+
+for x in range(11):
+    for y in range(11):
+        print(f"({x},{y})")
+
+
+f = [5,2,5,2,2]
+for no in f:
+    print("*" * no)
+print("Done")
+
+numbers =[1,2,3,4,5,6,7,8,9]
+tt = 0
+for noz in numbers:
+    tt += noz
+print(tt)
+
+find_max_number(numbers)

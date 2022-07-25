30879b25dbd6b65f677de06e278f5d3ae61ff3bc
allamatti98 <dayanallamatti98@gmail.com>
2022-07-25 08:27:24 +0300

    Multiple line strings

---
+++ Multiple Line Strings.py
@@ -0,0 +1,14 @@
+email = """"
+Dear Santa,
+You're a bitch nigga.
+
+I heard you put security around the mall to protect you.
+That was a bitch move Santa!!!
+
+I'm coming back to whoop that ass of yours.
+
+Your's truly,
+The Santa Stalker.
+
+"""
+print(email)

12467294d69e4b4bb32b3e9cf45aa655ebf36f26
allamatti98 <dayanallamatti98@gmail.com>
2022-07-28 22:14:22 +0300

    Range Function

---
+++ Range Function.py
@@ -0,0 +1,17 @@
+'''
+numbers = range(10)
+for item in numbers:
+    print (item)
+sequence = range(2, 20, 2) #third value is a step, skips according to what you want
+for number in sequence:
+    print(number)
+
+for no in range(10):
+    print(no)
+
+for no in range (10,15):
+    print (no)
+
+for no in range (10,20,2):
+    print(no)
+'''

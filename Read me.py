dc4dba7ab72bd84c9f534e9edd154ada3c4e482f
allamatti98 <dayanallamatti98@gmail.com>
2022-07-28 22:16:01 +0300

    Read me

---
+++ Read me.py
@@ -0,0 +1,20 @@
+#Welcome to the Pyhton manual.
+
+#Search for the Python 3 Math Module. to get details of all the Mathematical funcions
+
+#IDE stands for Integrated Development Environment. Basically a code editor on steroids.
+
+#PEP 8 provides best principles and stands for Pyhton Enhanced...
+
+#Search "Python 3 Module Index" for the details of all inbuilt python modules. Make sure to add the version number coz modules difffers basing on versions
+
+#PyPI (Python Package Index) This directory contains thousands of packages made by the Python Community. go to pypi.org
+#The are packages that help you access websites and search for things you want. You can also learn a technique called web-scraping, which means you can build an engine and have that engine browse a website and extract information for html files like how google indexes web files using a web crawler. You can also do browser automation, to test that a website is functioning properly for this use Selenium These can all be done with python
+#To install a package search on PyPI then copy and paste command in Terminal Window
+#pip is a tool that comes with python installation and we use it to install or uninstall packages regisered on PyPI(pypi.org)
+#WARNING!!! WARNING: You are using pip version 21.2.4; however, version 22.1.2 is available.
+#You should consider upgrading via the #'C:\Users\CYBERTECH\AppData\Local\Programs\Python\Python310\python.exe -m pip install --upgrade pip' command.
+
+#A function is made up of lines of code, functions make up a class, classes make up a module and modules make up a package.
+
+#Import data for machine learning in form of a csv file, this is the first step of Machine learning.

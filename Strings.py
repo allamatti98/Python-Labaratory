ab709bb7294029357ab6263634509b64ff038366
allamatti98 <dayanallamatti98@gmail.com>
2022-07-29 18:56:14 +0300

    Strings

---
+++ Strings.py
@@ -0,0 +1,42 @@
+Motto = "You Only Live Once"
+print(Motto)
+print(Motto[1])
+print(Motto.upper())
+print(Motto.split(" "))
+print(Motto.replace("Once", "Once and that's the Motto Nigga YOLO!!!"))
+print(Motto[0:6])
+print(Motto[:6])
+print(Motto[:])
+facts = Motto[:]
+print(facts)
+print(len(facts))
+notes = ("""
+Motto = "You Only Live Once"
+print(Motto.upper())
+print(Motto.find('O'))
+print(Motto.replace(Motto,"You Only Live Once and that's the motto Nigga YOLO!!!"))
+print("Only" in Motto)
+sd = "QWERTY"
+print(sd[-3])
+print(Motto[0])
+print(Motto[-6])
+print(Motto[0:8])
+print (Motto[9:])
+print(Motto[:13])
+print(Motto[:])
+#making a copy
+copy = Motto[:]
+print(copy)
+Facts = "Python is the best!!!"
+print(len(Facts))
+print(Facts.upper())
+print(Facts.lower())
+print(Facts.title())
+print(Facts.find('best'))
+print(Facts.replace('best','best no cap'))
+Facts.replace('best', 'best I am not even capping')
+print(Facts)
+print('the' in Facts)
+
+print(Motto.find(''))
+""")

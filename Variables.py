1783b2598c7e69b049fd2d36e98f482df99dfceb
allamatti98 <dayanallamatti98@gmail.com>
2022-07-31 12:07:48 +0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,12 @@
+name = input("What is your name?: ")
+print("Hello " + name + ", welcome to Kingsman Hospital.")
+age = input("How old are you?: ")
+bd = 2022 - int(age)
+print("I too was born in " + str(bd))
+while True:
+    is_new = input("Are you a new Patient?: ")
+    if is_new.upper() == "YES" or is_new.upper() == "NO":
+        print("File successfully saved")
+        break
+    else:
+        print(f"The answer is Yes or No {name}")

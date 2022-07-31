e431c01b11f5161d13a16048e1b0d9201d2f1b6f
allamatti98 <dayanallamatti98@gmail.com>
2022-07-31 12:06:38 +0300

    Unpacking

---
+++ Unpacking.py
@@ -0,0 +1,15 @@
+a,b,c,d,e = [4,3,5,6,7]
+print(c)
+
+Tuple = (1,2,3,4,5)
+v = Tuple[0]
+w = Tuple[1]
+x = Tuple[2]
+y = Tuple[3]
+z = Tuple[4]
+List =[6,7,8,9,10]
+
+#Unpacking
+a,b,c,d,e = List
+print(x)
+print(d)

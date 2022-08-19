d107495eb3c6cae0eb8cc99d296df19b65f8c68b
allamatti98 <dayanallamatti98@gmail.com>
2022-08-19 18:08:28 +0300

    Trim

--- Variables.py
+++ Variables.py
@@ -10,36 +10,3 @@ while True:
         break
     else:
         print("Type 'Yes' or 'No'")
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-name = input("What is your name?: ")
-print("Hello " + name + ", welcome to Kingsman Hospital.")
-age = input("How old are you?: ")
-bd = 2022 - int(age)
-print("I too was born in " + str(bd))
-while True:
-    is_new = input("Are you a new Patient?: ")
-    if is_new.upper() == "YES" or is_new.upper() == "NO":
-        print("File successfully saved")
-        break
-    else:
-        print(f"The answer is Yes or No {name}")
-'''

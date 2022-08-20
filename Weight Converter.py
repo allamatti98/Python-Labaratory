a367a8889fa4b23a6364d38b8c9d60227c17291b
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 12:15:49 +0300

    right figures

--- Weight Converter.py
+++ Weight Converter.py
@@ -2,11 +2,11 @@ weight = input("What is your weight? ")
 while True:
     metrics = input("Is it in Kilograms (Kg) or Pounds(Lbs)")
     if metrics.upper() == "KGS":
-        pounds = float(weight) /1.6
+        pounds = float(weight) /0.45
         print(f'Your weight in pounds is {pounds}')
         break
     elif metrics.lower() == "lbs":
-        kilograms = float(weight) * 1.6
+        kilograms = float(weight) * 0.45
         print("Your weight in Kilograms is " + kilograms)
         break
     else:

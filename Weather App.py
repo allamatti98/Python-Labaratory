13fe08039d04bbdc95bb27139efa3791c661e453
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 14:33:11 +0300

    Weather inquiry

---
+++ Weather App.py
@@ -0,0 +1,7 @@
+temp = input("What is the temperature in your city? ")
+if temp >= 30:
+    print("Drink plenty of water ")
+elif temp <= 15:
+    print("Wear warm clothes ")
+else:
+    print("It's a lovely day")

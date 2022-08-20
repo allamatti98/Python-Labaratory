f5f634fef34a7932a94634c535c132a199fbe128
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 21:44:02 +0300

    Remove repeated nos

--- Lists.py
+++ Lists.py
@@ -1,3 +1,42 @@
+l = ["John", "Simon", "Peter", "Luke", "Mathew","Emiliana"]
+max = 0
+lname = ""
+for name in l:
+    if len(name) > max:
+        lname = name
+print(name)
+
+list = [1,2,3,4,5]
+list.append(10)
+list.insert(0,0)
+list.remove(10)
+list.clear()
+
+numb = [1,2,3,3,3,4,5,5,6,7,8,9]
+numb2 = []
+for no in numb:
+    if no not in numb2:
+        numb2.append(no)
+print(numb2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 ballers = ["Kevin", "Sterling", "Grielish", "Fernandinho"]
 print (ballers [0])
 print (ballers[0:3])
@@ -15,3 +54,4 @@ print(max(sets))
 print(mode(sets))
 
 numbers = (1,2,3,4,5,6,7,8,9,10)
+"""

5193be9c689b6ce444b10821585605204cb401e9
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 12:13:44 +0300

    Tweaks

--- Weight Converter.py
+++ Weight Converter.py
@@ -1,7 +1,7 @@
 weight = input("What is your weight? ")
 while True:
     metrics = input("Is it in Kilograms (Kg) or Pounds(Lbs)")
-    if metrics.upper() == "Kgs":
+    if metrics.upper() == "KGS":
         pounds = float(weight) /1.6
         print(f'Your weight in pounds is {pounds}')
         break
@@ -11,68 +11,3 @@ while True:
         break
     else:
         print("Type 'Kgs' or 'Lbs'")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-while True:
-    weight = float(input("Kindly enter your weight: "))
-    unit = input("Is it in Kilograms (Kgs) or Pounds(Lbs)?: ")
-    if unit.upper() == "KGS":
-        weight * 0.45
-        print("Your weight in Pounds is " + str(weight) + "Lbs.")
-        break
-    elif unit.upper() == "LBS":
-        weight/0.45
-        print("Your weight in Kilograms is " + str(weight) +"Kgs")
-        break
-    else:
-        print("Type 'Kgs' or 'Lbs'")
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-weight = input("What is your weight?: ")
-while True:
-    unit = input("Is it in Kilograms(Kgs) or Pounds(Lbs)? ")
-    if unit.upper() == "KGS":
-        pounds = float(weight) / 0.45
-        print(f"Your weight in Pounds is {pounds}")
-        break
-    elif unit.upper() == "POUNDS":
-        kilograms = float(weight) * 0.45
-        print("Your weight in Kilograms is " + kilograms)
-        break
-'''

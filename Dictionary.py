bdd7e935129869c850bc0482d530c5e40d7a4c8e
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 22:57:02 +0300

    word changer

--- Dictionary.py
+++ Dictionary.py
@@ -24,54 +24,14 @@ for no in contact:
     output += dic1.get(no) + " "
 print(output)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-biodata = {
-    "First_Name": "Alex",
-    "Last_Name": "Hunter",
-    "Age": 23,
-    "Striker": True
+dic2 = {
+    "cool": "dope",
+    "understand": "feel",
+    "those": "dem"
 }
-
-print(biodata)
-print(biodata["Striker"])
-print(biodata["Last_Name"])
-#print(biodata["School"])
-print(biodata.get("birthdate","1st July 1999"))
-
-
-#Challenge
-numbers = {
-    "0": "Zero ",
-    "1": "One ",
-    "2": "Two ",
-    "3": "Three ",
-    "4": "Four ",
-    "5": "Five ",
-    "6": "Six ",
-    "7": "Seven ",
-    "8": "Eight ",
-    "9": "Nine "
-}
-contact = input("Kindly enter your contact: ")
-for number in contact:
-    print(numbers[number])
-
-#MstAns
-output = ""
-for number in contact:
-    output += numbers.get(number, "!") + " "
-print(output)
-"""
+output2 = ""
+text = input("Type in text :")
+t2 = text.split()
+for key in t2:
+    output2 += dic2.get(key, key) + " "
+print(output2)

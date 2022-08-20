fb41f5103cd1dbaef532c75f1ec9c8be71590834
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 16:24:08 +0300

    Guessing 1.1

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,4 +1,14 @@
 #Make a program that gives a player 3 chances to guess a magic number.
+for i in range(3):
+    answer = input("Guess the secret number: ")
+    if answer == "9":
+        print("Good Job!!!")
+        break
+    else:
+        print("Wrong answer champ.")
+else:
+    print("Sorry, you're out of chances. :(")
+
 
 
 

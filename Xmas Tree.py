cb2827309ab00ed2a556463358b61866c92f00ad
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 12:36:13 +0300

    function

--- Xmas Tree.py
+++ Xmas Tree.py
@@ -1,8 +1,17 @@
-output = ""
-s = 8
-a = 0
-while a <= 8:
-    spaces = " " * s
-    stars = "*" * a
-    output = spaces + stars
-    print(output)
+def star_tree()
+    import time
+    output = ""
+    s = 8
+    a = 1
+    while a <= 13:
+        spaces = " " * s
+        stars = "*" * a
+        output = spaces + stars
+        print(output)
+        s -= 1
+        a += 2
+    for i in range(4):
+        print (" "*7 + "*"*2)
+        time.sleep(1)
+
+star_tree()

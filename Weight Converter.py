5c2dd1e56a3774a75a715943d9f4c7d13e15c2f9
allamatti98 <dayanallamatti98@gmail.com>
2022-08-20 12:06:18 +0300

    breaks

--- Weight Converter.py
+++ Weight Converter.py
@@ -1,13 +1,16 @@
 weight = input("What is your weight? ")
-metrics = input("Is it in Kilograms (Kg) or Pounds(Lbs)")
-if metrics.upper() == "Kgs":
-    pounds = float(weight) /1.6
-    print(f'Your weight in pounds is {pounds}')
-elif metrics.lower() == "lbs":
-    kilograms = float(weight) * 1.6
-    print("Your weight in Kilograms is " + kilograms)
-else:
-    print("Type 'Kgs' or 'Lbs'")
+while True:
+    metrics = input("Is it in Kilograms (Kg) or Pounds(Lbs)")
+    if metrics.upper() == "Kgs":
+        pounds = float(weight) /1.6
+        print(f'Your weight in pounds is {pounds}')
+        break
+    elif metrics.lower() == "lbs":
+        kilograms = float(weight) * 1.6
+        print("Your weight in Kilograms is " + kilograms)
+        break
+    else:
+        print("Type 'Kgs' or 'Lbs'")
 
 
 

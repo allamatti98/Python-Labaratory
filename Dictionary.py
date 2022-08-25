b7483a6487965922c1f887ea168d628c75d27ea2
allamatti98 <dayanallamatti98@gmail.com>
2022-08-25 23:33:25 +0300

    number to word converter

--- Dictionary.py
+++ Dictionary.py
@@ -5,7 +5,23 @@ biodata = {
 }
 print(biodata["name"])
 
-
+numbers = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    }
+output = ""
+contact = input("Kindly enter your contact: ")
+for no in contact:
+    output += numbers.get(no, "!") + " "
+print(output)
 
 
 

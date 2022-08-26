4a14ae04c2893586466e9394bd3a543688a6c20e
allamatti98 <dayanallamatti98@gmail.com>
2022-08-26 18:59:02 +0300

    import random

--- GenRandomValues.py
+++ GenRandomValues.py
@@ -1,15 +1,12 @@
 #Generate random values between 0 and 1, then values between any range you specify.
 #Then pick a string at random
 import random
-for i in range(9):
-    print(random.random())
 
-for i in range (9):
-    print(random.randint(10,20))
 
-cities = ['London', 'New York', 'Los Angeles']
-for i in range(9):
-    print(random.choice(cities))
+
+
+
+
 
 
 

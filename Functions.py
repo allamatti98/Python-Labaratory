b6d93517ad4d12fe6f9f3ff38abe03a77006917a
allamatti98 <dayanallamatti98@gmail.com>
2022-08-26 08:19:15 +0300

    Greet User

--- Functions.py
+++ Functions.py
@@ -9,35 +9,7 @@ def lambo_diagnosis():
     Radio On
     """)
 
+def greet_user(name):
+    print("Hello " + name)
 
-def error_message():
-    print("Command not recognized!!!")
-
-Status = "Off"
-while True:
-    command = input(">")
-    if command.upper() == "START" and Status == "Off":
-        print("Engine Started...")
-        lambo_diagnosis()
-        Status = "On"
-    elif command.upper() == "START" and Status == "On":
-        print("Engine is already running, ready to go. Press ESC for Launch Control")
-    elif command.upper() == "STOP" and Status == "On":
-        print("Closing pump Valve...")
-        print("Engine off")
-        Status = "Off"
-    elif command.upper() == "STOP" and Status == "Off":
-        print("Engine is already off")
-    elif command.upper() == "HELP":
-        print("""
-        Start -> Use this to start the Lambo Engine
-        Stop -> Use this to stop the Lambo Engine
-        Help -> Use this for help menu
-        Quit -> Use this to exit remote Control
-        """)
-    elif command.upper() == "QUIT":
-        break
-    else:
-        error_message()
-
-
+greet_user("Peter")
